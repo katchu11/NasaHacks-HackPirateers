7c6d252a032364c927ca164b80593add3657c9da
katchu11 <katchu11@gmail.com>
2017-04-29 15:01:47 -0400

    Update Establish_SQL_Connection.py

--- Establish_SQL_Connection.py
+++ Establish_SQL_Connection.py
@@ -1,9 +1,12 @@
 import mysql.connector
 
-
+#Put the user, password , the host IPv4 and the database name
 con =  mysql.connector.connect(user = 'root',password = 'root', host = '127.0.0.1',database = 'Nasa')
+#Create cursor to execute commands
 cursor = con.cursor()
+#SQL Query
 cursor.execute("Select * from state_data")
+#Fetch All Rows
 data = cursor.fetchall ()
 # print the rows
 for row in data :
